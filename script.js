96af03496b16bb3add720b2966c9640e9b24e33a
Back2life02 <darcookies@hotmail.com>
2025-05-03 22:49:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const images = document.querySelectorAll(".galeria-columna img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  lightbox.style.display = "block";
+  lightboxImg.src = images[index].src;
+  currentIndex = index;
+}
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => showImage(index));
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+prevBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+});
+
+nextBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  lightboxImg.src = images[currentIndex].src;
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) lightbox.style.display = "none";
+});
+const toggleBtn = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  toggleBtn.classList.toggle("is-active"); // activa la animación hamburguesa
+});
